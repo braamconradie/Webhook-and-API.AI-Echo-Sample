e8a493097aa5a37dd1eb2e08cd218db1d06f7b28
braamconradie <braam.conradie@gmail.com>
2017-09-09 16:00:45 +1200

    Update index.js
    included if statements

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    speech = 'yster';  // braam tries to over ride
+    if (speech == "horing"){
+       speech="lekker horing";
+    } else if (speech == "draad"){
+        speech = "lekker draad";
+    }
+    // speech = 'yster';  // braam tries to over ride
     return res.json({
         speech: speech,
         displayText: speech,
