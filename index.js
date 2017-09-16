baa3d5cf24d6efc3e786de02abc5efaad654706f
braamconradie <braam.conradie@gmail.com>
2017-09-16 15:10:42 +1200

    Update index.js
    block in line 17 to test the waters with dweet

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    
+    //try to send dweet
+    theUrl = 'https://dweet.io/dweet/for/innerbowel1?hello=jaekishier'
+    function httpGet(theUrl)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+        return xmlHttp.responseText;
+    }
+    httpGet(theUrl);
+    
+    
+    
     return res.json({
         speech: speech,
         displayText: speech,
