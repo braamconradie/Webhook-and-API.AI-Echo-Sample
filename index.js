77c88c8384a4535dffec962f29221b048278ccb0
braamconradie <braam.conradie@gmail.com>
2017-09-16 21:50:51 +1200

    Update index.js

--- index.js
+++ index.js
@@ -35,9 +35,10 @@ restService.post('/echo', function(req, res) {
 const request = require('request');
     
     
-stuurdit = 'https://dweet.io/dweet/for/braam666?hello=stuurdit';
+var stuurdit = 'https://dweet.io/dweet/for/braam666?hello=stuurdit';
 //request('https://dweet.io/dweet/for/braam666?hello=world', function (error, response, body) {
-// request(stuurdit, function (error, response, body) {
+
+request(stuurdit, function (error, response, body) {
    console.log('error:', error); // Print the error if one occurred
    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
    console.log('body:', body); // Print the HTML for the Google homepage.
