6f727d176fff904fba5c852e51fb0bc1c7adef9e
braamconradie <braam.conradie@gmail.com>
2017-09-16 21:36:15 +1200

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ restService.post('/echo', function(req, res) {
       if (speech == "horing"){
         speech="lekker horing";
           
-        request('https://dweet.io/dweet/for/braam666?hello=lekkerhoring', function (error, response, body) {
+        /*
+          request('https://dweet.io/dweet/for/braam666?hello=lekkerhoring', function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
             });
-            
+        */    
           
          } else if (speech == "draad"){
                      speech = "lekker draad";
