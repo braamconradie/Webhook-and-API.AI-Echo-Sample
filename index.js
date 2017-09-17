c4ad3a1e09643e8768fab69a813224a4809c1301
braamconradie <braam.conradie@gmail.com>
2017-09-17 19:10:45 +1200

    Update index.js
    try block below line 33

--- index.js
+++ index.js
@@ -28,9 +28,18 @@ restService.post('/echo', function(req, res) {
             });
         */    
           
-         } else if (speech == "draad"){
-                     speech = "lekker draad";
+         } else if (speech == "braampower"){
              
+             // get reading from dweet
+                 request('https://dweet.io/get/latest/dweet/for/braamwatts', function (error, response, body) {
+                     console.log ('**************');
+                     //console.log ('body is ' + body);
+                     var myJSON = JSON.parse(body);
+                     // console.log (myJSON);
+                     var tweedeobj = myJSON["with"];
+                     console.log(tweedeobj[0]["content"]["totalkrag"]);
+                  });
+
          } else if (speech == "lancevid1"){
                      speech = "lancevid1";
        }
